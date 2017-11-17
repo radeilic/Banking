2f77f72542d5628474e810557b5f6bfa947da96b
kbjxkgx <33682422+kbjxkgx@users.noreply.github.com>
2017-11-17 09:09:44 +0100

    Finalized methods for puting requests in queues

--- Banking/Common/Services/IUserServices.cs
+++ Banking/Common/Services/IUserServices.cs
@@ -10,12 +10,30 @@ namespace Common.Services
     [ServiceContract]
     public interface IUserServices
     {
+        /// <summary>
+        /// Opening account
+        /// </summary>
+        /// <param name="accountName"></param>
+        /// <returns></returns>
         [OperationContract]
         bool OpenAccount(string accountName);
 
+        /// <summary>
+        /// Loaning money amouunt
+        /// </summary>
+        /// <param name="accountName"></param>
+        /// <param name="amount">Amount of money for loan</param>
+        /// <returns></returns>
         [OperationContract]
         bool RaiseALoan(string accountName, int amount);
 
+        /// <summary>
+        /// Payment method
+        /// </summary>
+        /// <param name="isPayment">If is true than is +payment else is -payment</param>
+        /// <param name="accountName"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         [OperationContract]
         bool Payment(bool isPayment, string accountName, int amount);
         
