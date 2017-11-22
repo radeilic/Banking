1473937c87d862a41ebed97b74035bda2da0f0c6
kbjxkgx <33682422+kbjxkgx@users.noreply.github.com>
2017-11-22 10:12:44 +0100

    Update.

---
+++ Banking/Common/Services/IBankingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Services
+{
+    [ServiceContract]
+    public interface IBankingService
+    {
+        [OperationContract]
+        bool Check(Request request);
+    }
+}
---
+++ Banking/IDS/BankingServiceIDS.cs
@@ -0,0 +1,18 @@
+﻿using Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace IDS
+{
+    public class BankingServiceIDS : IBankingService
+    {
+        public bool Check(Request request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
